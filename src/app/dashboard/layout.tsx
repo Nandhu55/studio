45ea5f2bb7e67000b5e86c4cc3a6515ec3547aa9
G NANDHISHWAR REDDY <gnreddy255@gmail.com>
2025-07-21 11:47:39 +0000

    admin page  is not working properly Like it is not opening homepage and

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -11,7 +11,8 @@ export default function AppLayout({ children }: { children: React.ReactNode }) {
   useEffect(() => {
     if (typeof window !== 'undefined') {
       const isLoggedIn = sessionStorage.getItem('isLoggedIn');
-      if (isLoggedIn !== 'true') {
+      const isAdmin = sessionStorage.getItem('isAdmin');
+      if (isLoggedIn !== 'true' && isAdmin !== 'true') {
         router.replace('/login');
       }
     }
